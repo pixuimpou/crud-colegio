3faf2338e034aa6cfed37056ee6b0162c642c3c0
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2021-02-09 19:25:22 -0300

    funcao perguntas e respostas

--- script.js
+++ script.js
@@ -14,25 +14,103 @@
 // Argumento => É / São o(s) valor(es) que precisa(m) ser passado(s) para obter o resultado de uma função.
 
 
-const piDeXFor = (num, counter) => {
-    for(let i = 1; i <= counter; i++) {
-        if(i % num === 0 || (i - num) % 10 === 0){
-            console.log("Pi");
-        } else {
-            console.log(i);
-        }
+// const piDeXFor = (num, counter) => {
+//     for(let i = 1; i <= counter; i++) {
+//         if(i % num === 0 || (i - num) % 10 === 0){
+//             console.log("Pi");
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// const piDeXWhile = (num, counter) => {
+//     let i = 1;
+
+//     while (i <= counter) {
+//         if (i % num === 0 || (i - num) % 10 === 0) {
+//             console.log("Pi");
+//         } else {
+//             console.log(i);
+//         }
+//         i++
+//     }
+// }
+
+let perguntas = [
+    {
+        id: 1,
+        label: "Qual é a minha cor favorita?",
+        opcoes: [
+            { id: 1, label: "amarelo" },
+            { id: 2, label: "vermelho" },
+            { id: 3, label: "verde" },
+            { id: 4, label: "rosa" }
+        ]
+    },
+    {
+        id: 2,
+        label: "Qual é o meu nome?",
+        opcoes: [
+            { id: 1, label: "Rafael Marcos" },
+            { id: 2, label: "Marcos Rafael" },
+            { id: 3, label: "Marcos" },
+            { id: 4, label: "Rafael" }
+        ]
+    },
+    {
+        id: 3,
+        label: "Qual é a melhor forma cientificamente comprovada de chamar o Toy?",
+        opcoes: [
+            { id: 1, label: "Totoy" },
+            { id: 2, label: "Apenas toy" },
+            { id: 3, label: "Toyzinho" },
+            { id: 4, label: "Cachorro" }
+        ]
     }
+];
+let respostas = [
+    { idPergunta: 1, idOpcao: 3 },
+    { idPergunta: 2, idOpcao: 4 },
+    { idPergunta: 3, idOpcao: 1 },
+
+];
+
+const Fazergabarito = () => {
+    let gabarito = [];
+
+    respostas.forEach(resposta => {
+        let idResposta = resposta.idPergunta;
+        let verificadorPergunta = false;
+        let verificadorOpcao = false;
+        perguntas.forEach(pergunta => {
+            let labelOpcao;
+            let respostaDaPergunta = pergunta.id === idResposta;
+            
+
+            if(respostaDaPergunta) {
+                
+                verificadorPergunta = true;
+                pergunta.opcoes.forEach(opcao => {
+                    let opcaoCerta = resposta.idOpcao === opcao.id;
+
+                    if(opcaoCerta) {
+                        verificadorOpcao = true;
+                        labelOpcao = opcao.label;
+                        gabarito.push({ id: idResposta, label: pergunta.label, resposta: labelOpcao, idOpcao: resposta.idOpcao })
+                    }
+                })     
+            }
+            
+        })
+        if (verificadorOpcao === false) {
+            console.log("A pergunta com id:", idResposta, "não possui opção com id:", resposta.idOpcao);
+        }
+        if(verificadorPergunta === false){
+            console.log("A pergunta com id:",idResposta,"não existe")
+        }
+    });
+    console.log(gabarito);
 }
 
-const piDeXWhile = (num, counter) => {
-    let i = 1;
-
-    while (i <= counter) {
-        if (i % num === 0 || (i - num) % 10 === 0) {
-            console.log("Pi");
-        } else {
-            console.log(i);
-        }
-        i++
-    }
-}
+Fazergabarito()
